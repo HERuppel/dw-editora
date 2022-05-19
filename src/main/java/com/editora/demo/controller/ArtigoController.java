50afcb5a5e52f6e09837a7f530c8db295e86145c
Renato Ruppel <heruppel2@gmail.com>
2022-05-19 20:43:34 -0300

    code formatting and added search by keyword in get method

--- src/main/java/com/editora/demo/controller/ArtigoController.java
+++ src/main/java/com/editora/demo/controller/ArtigoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.editora.demo.entity.Artigo;
@@ -31,45 +31,49 @@ public class ArtigoController {
   }
 
   @GetMapping() // LISTAR TODOS
-  public List<Artigo> Get() {
+  public List<Artigo> Get(@RequestParam(required = false) String titulo) {
+    if (titulo == null) {
       return _artigoRepository.findAll();
+    }
+    List<Artigo> artigo = _artigoRepository.findByKeyword(titulo);
+    return artigo;
   }
 
   @GetMapping("/{id}") // RECUPERAR POR ID
   public ResponseEntity<Artigo> GetById(@PathVariable("id") long id) {
     Optional<Artigo> artigo = _artigoRepository.findById(id);
     if(artigo.isPresent())
-        return new ResponseEntity<Artigo>(artigo.get(), HttpStatus.OK);
+      return new ResponseEntity<Artigo>(artigo.get(), HttpStatus.OK);
     else
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @PutMapping("/{id}") // ATUALIZAR POR ID
   public ResponseEntity<Artigo> Put(@PathVariable("id") long id, @RequestBody Artigo newArtigo) {
-      Optional<Artigo> oldArtigo = _artigoRepository.findById(id);
-      if(oldArtigo.isPresent()) {
-          Artigo artigo = oldArtigo.get();
+    Optional<Artigo> oldArtigo = _artigoRepository.findById(id);
+    if (oldArtigo.isPresent()) {
+      Artigo artigo = oldArtigo.get();
 
-          artigo.setTitulo(newArtigo.getTitulo());
-          artigo.setResumo(newArtigo.getResumo());
-          artigo.setStatus(newArtigo.getStatus());
+      artigo.setTitulo(newArtigo.getTitulo());
+      artigo.setResumo(newArtigo.getResumo());
+      artigo.setStatus(newArtigo.getStatus());
 
-          _artigoRepository.save(artigo);
-          return new ResponseEntity<Artigo>(artigo, HttpStatus.OK);
-      }
-      else
-          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      _artigoRepository.save(artigo);
+      return new ResponseEntity<Artigo>(artigo, HttpStatus.OK);
+    }
+    else
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @DeleteMapping("/{id}") // DELETAR POR ID
   public ResponseEntity<Object> DeleteById(@PathVariable("id") long id) {
-      Optional<Artigo> artigo = _artigoRepository.findById(id);
-      if(artigo.isPresent()){
-          _artigoRepository.delete(artigo.get());
-          return new ResponseEntity<>(HttpStatus.OK);
-      }
-      else
-          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    Optional<Artigo> artigo = _artigoRepository.findById(id);
+    if(artigo.isPresent()){
+      _artigoRepository.delete(artigo.get());
+      return new ResponseEntity<>(HttpStatus.OK);
+    }
+    else
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @DeleteMapping() // DELETAR POR ID
@@ -80,7 +84,6 @@ public class ArtigoController {
 
   @GetMapping("/publicados") // LISTAR TODOS OS PUBLICADOS
   public List<Artigo> GetPublished() {
-      return _artigoRepository.findPublished();
+    return _artigoRepository.findPublished();
   }
-
 }
