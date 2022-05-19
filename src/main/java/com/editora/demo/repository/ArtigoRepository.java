9d5e0800d38cae9a6c913abbd5c1706108468106
Renato Ruppel <heruppel2@gmail.com>
2022-05-19 19:50:08 -0300

    implemented findPublished in repository

--- src/main/java/com/editora/demo/repository/ArtigoRepository.java
+++ src/main/java/com/editora/demo/repository/ArtigoRepository.java
@@ -1,9 +1,16 @@
 package com.editora.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import com.editora.demo.entity.Artigo;
 
 @Repository
-public interface ArtigoRepository extends JpaRepository<Artigo, Long> { }
+public interface ArtigoRepository extends JpaRepository<Artigo, Long> { 
+
+  @Query(value = "select * from artigo where status = true ", nativeQuery = true)
+  public List<Artigo> findPublished();
+}
