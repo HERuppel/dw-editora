156dd3ca2b12f1925b03cdcf8a71f4b49a4e6574
Renato Ruppel <heruppel2@gmail.com>
2022-05-19 20:43:03 -0300

    added findByKeyword query to repository

--- src/main/java/com/editora/demo/repository/ArtigoRepository.java
+++ src/main/java/com/editora/demo/repository/ArtigoRepository.java
@@ -2,6 +2,7 @@ package com.editora.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import com.editora.demo.entity.Artigo;
 @Repository
 public interface ArtigoRepository extends JpaRepository<Artigo, Long> { 
 
-  @Query(value = "select * from artigo where status = true ", nativeQuery = true)
+  @Query(value = "select * from artigo where status = true", nativeQuery = true)
   public List<Artigo> findPublished();
+
+  @Query(value = "select * from artigo where titulo like %:titulo%", nativeQuery = true)
+  public List<Artigo> findByKeyword(@Param("titulo") String titulo);
 }
